03c94bf147183be6615ade46e701d0878b421451
Tabitoo <lautarovaldez4@gmail.com>
2023-09-22 11:24:50 -0300

    Avance en la funcionalidad de materia

--- Intraconsulta/test/ar/unlam/intraconsulta/TestMateria.java
+++ Intraconsulta/test/ar/unlam/intraconsulta/TestMateria.java
@@ -7,8 +7,40 @@ import org.junit.Test;
 public class TestMateria {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSePuedaAgregarUnaMateriaALaUniverdidad() {
+		Universidad unlam = new Universidad("unlam");
+		Materia mobil = new Materia("mobil 1", 12);
+		
+		assertTrue(unlam.registraMateria(mobil));
 	}
+	
+	
+	@Test
+	public void queNoSePuedaAgregarDosMateriasConElMismoId() {
+		Universidad unlam = new Universidad("unlam");
+		Materia mobil = new Materia("mobil 1", 12);
+		Materia mobil2 = new Materia("mobil 2", 12);
+		
+		assertTrue(unlam.registraMateria(mobil));
+		assertFalse(unlam.registraMateria(mobil2));
+		
+		
+	}
+	
+	@Test
+	public void queSePuedaAgregarCorrelativa() {
+		
+		Universidad unlam = new Universidad("unlam");
+		Materia mobil = new Materia("mobil 1", 12);
+		Materia mobil2 = new Materia("mobil 2", 12);
+		
+		assertTrue(unlam.registraMateria(mobil));
+		assertFalse(unlam.registraMateria(mobil2));
+		
+		
+	}
+	
+	
+	
 
 }
