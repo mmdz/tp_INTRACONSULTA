53de9bad36c01c6a8ff12c33474aa0ac8f00cb4a
Lautaro Valdez <57192096+Tabitoo@users.noreply.github.com>
2023-09-23 13:38:04 -0300

    Se agrego la funcionalidades de correlatividad

--- Intraconsulta/test/ar/unlam/intraconsulta/TestMateria.java
+++ Intraconsulta/test/ar/unlam/intraconsulta/TestMateria.java
@@ -32,15 +32,51 @@ public class TestMateria {
 		
 		Universidad unlam = new Universidad("unlam");
 		Materia mobil = new Materia("mobil 1", 12);
-		Materia mobil2 = new Materia("mobil 2", 12);
+		Materia mobil2 = new Materia("mobil 2", 13);
 		
 		assertTrue(unlam.registraMateria(mobil));
-		assertFalse(unlam.registraMateria(mobil2));
+		assertTrue(unlam.registraMateria(mobil2));
+		
+		assertTrue(unlam.agregarCorrelatividad(mobil2.getId(), mobil.getId()));
 		
 		
 	}
 	
+	@Test
+	public void queNoPuedaAgregarCorrelativa() {
+		
+		Universidad unlam = new Universidad("unlam");
+		Materia mobil = new Materia("mobil 1", 12);
+		Materia mobil2 = new Materia("mobil 2", 13);
+		
+		assertTrue(unlam.registraMateria(mobil));
+		assertTrue(unlam.registraMateria(mobil2));
+		
+		assertFalse(unlam.agregarCorrelatividad(mobil2.getId(), 10));
+		
+		
+	}
 	
+	@Test
+	public void queSePuedaEliminarCorrelativa() {
+		
+		Universidad unlam = new Universidad("unlam");
+		Materia mobil = new Materia("mobil 1", 12);
+		Materia mobil2 = new Materia("mobil 2", 13);
+		
+		assertTrue(unlam.registraMateria(mobil));
+		assertTrue(unlam.registraMateria(mobil2));
+		
+		assertTrue(unlam.agregarCorrelatividad(mobil2.getId(),  mobil.getId()));
+		
+		//System.out.println(mobil2.getCorrelativa());
+		
+		
+		assertTrue(unlam.eliminarCorrelatividad(mobil2.getId(),  mobil.getId()));
+		
+		//System.out.println(mobil2.getCorrelativa());
+			
+	}
 	
 
 }
